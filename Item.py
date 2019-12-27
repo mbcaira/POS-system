194c0dbc0670bae3787b65d0b11c7e66bfc1e677
michael-canaran <58964071+michael-canaran@users.noreply.github.com>
2019-12-27 01:23:54 -0500

    Add files via upload

--- Item.py
+++ Item.py
@@ -1,8 +1,45 @@
+#Global tax rate
+taxRate = 1.13
+
 #Data fields used for the item class
 class Item:
-    def __init__(self, number, name, price, taxed, tax):
+    def __init__(self, number, name, price, taxed):
         self.number = number
         self.name = name
         self.price = price
         self.taxed = taxed
-        self.tax = tax
+        if self.taxed == "N":
+            self.tax = 0
+        else:
+            self.tax = taxRate*self.price
+    
+    #Method to pull item data from the inventory list and make an item object
+    def checkInventory(self):
+        inv = open("inventory.txt", "r")
+        inventory = []
+        for i in inv:
+            inventory.append(i)
+        #Removes text headers
+        inventory.pop(0)
+        
+        #Search up item input using item number and if it exists on the inventory list, make an item object
+        if self.number in inventory:
+            
+            #Gather item info from inventory array if the item exists
+            number = self.number
+            itemPos = inventory.index(number)
+            name = inventory[itemPos][9:19]
+            price = inventory[itemPos][25:30]
+            taxed = inventory[itemPos][33]
+
+            memberItem = Item(number, name, price, taxed)
+            return(memberItem)
+        else:
+            print("ITEM NOT FOUND")
+
+            
+
+
+    
+
+        
--- POS.py
+++ POS.py
@@ -3,5 +3,4 @@ import Membership
 import Receipt
 
 class POS:
-    mem = Membership.Membership("Sarah", 132, 2020)
-    print(Membership.Members[0].name)
+    
